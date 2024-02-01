571dfaf812d3258ee082b5ead3b2c4f59f6a2131
Madina <madinatussupova0820@gmail.com>
2024-02-01 14:03:50 +0600

    update job form

--- src/components/JobForm.jsx
+++ src/components/JobForm.jsx
@@ -1,38 +1,49 @@
 // JobForm.js
-import React, { useState } from 'react';
-import '../index.css';
+import React, { useState } from "react";
+import "../index.css";
 
 function JobForm({ onAddJob }) {
-  const [company, setCompany] = useState('');
-  const [position, setPosition] = useState('');
-  const [dateApplied, setDateApplied] = useState('');
+ const [company, setCompany] = useState("");
+ const [position, setPosition] = useState("");
+ const [dateApplied, setDateApplied] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newApplication = { company, position, dateApplied };
-    onAddJob(newApplication);
-    setCompany('');
-    setPosition('');
-    setDateApplied('');
-  };
+ const handleSubmit = (e) => {
+  e.preventDefault();
+  const newApplication = { company, position, dateApplied };
+  onAddJob(newApplication);
+  setCompany("");
+  setPosition("");
+  setDateApplied("");
+ };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Company:
-        <input type="text" value={company} onChange={(e) => setCompany(e.target.value)} />
-      </label>
-      <label>
-        Position:
-        <input type="text" value={position} onChange={(e) => setPosition(e.target.value)} />
-      </label>
-      <label>
-        Date Applied:
-        <input type="date" value={dateApplied} onChange={(e) => setDateApplied(e.target.value)} />
-      </label>
-      <button type="submit">Add Job</button>
-    </form>
-  );
+ return (
+  <div className="formContainer">
+   <form onSubmit={handleSubmit}>
+    <label htmlFor="id">Company:</label>
+    <input
+     id="company"
+     type="text"
+     value={company}
+     onChange={(e) => setCompany(e.target.value)}
+    />
+    <label htmlFor="position">Position:</label>
+    <input
+     id="position"
+     type="text"
+     value={position}
+     onChange={(e) => setPosition(e.target.value)}
+    />
+    <label htmlFor="date"> Date Applied:</label>
+    <input
+     id="date"
+     type="date"
+     value={dateApplied}
+     onChange={(e) => setDateApplied(e.target.value)}
+    />
+    <button type="submit">Add Job</button>
+   </form>
+  </div>
+ );
 }
 
 export default JobForm;
