59998b2d68ff122f9236057fe7c5c3aa7ed77815
Debi <jenadebiprasad6@gmail.com>
2024-02-11 18:29:24 +0530

    Displayed jobItems

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -1,9 +1,41 @@
+import day from "../assets/day.svg"
+import building from "../assets/building.svg";
+import position from "../assets/position.svg";
+import briefcase from "../assets/briefcase.svg";
+import list from "../assets/list.svg";
+
 function JobItem({ job }) {
  return (
-  <li>
-   <strong>{job.company}</strong> - {job.position} (Applied on {job.dateApplied}
-   )
-  </li>
+   <div className="jobitem">
+     <div>
+       <div>
+         <img src={position} alt="" />
+         <span>{job.position}</span>
+       </div>
+
+       <div>
+         <img src={building} alt="" />
+         <span>{job.company}</span>
+       </div>
+     </div>
+
+     <div>
+       <div>
+         <img src={briefcase} alt="" />
+         <span>{job.jobtype}</span>
+       </div>
+
+       <div>
+         <img src={day} alt="" />
+         <span>{job.dateApplied}</span>
+       </div>
+     </div>
+     <div  id="status" >
+       <img src={list} alt="" />
+       <span>{job.status}</span>
+     </div>
+     
+   </div>
  );
 }
 
