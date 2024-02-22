5ffd743a84e3b7eea9a183bb07a9a4bcc977de21
Madina <madinatussupova0820@gmail.com>
2024-02-22 09:29:20 +0600

    update about description

--- src/components/layout/About.jsx
+++ src/components/layout/About.jsx
@@ -2,7 +2,7 @@ function About () {
   return (
       <div>
         <h2>About</h2>
-        <p>JobTrackr is an application to keep track of your job applications. Made with love. &#x1F497;</p>
+        <p>JobTrackr is created to keep track of your job applications. Made with love. &#x1F497;</p>
       </div>
   );
 };
