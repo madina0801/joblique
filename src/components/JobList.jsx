b37313a1ead2839c258cf17c2d52135d2f29fa5d
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-02-22 22:34:31 +0300

    added delete btn to job item component

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,16 +1,16 @@
 import JobItem from "./JobItem";
 
 function JobList({ jobApplications }) {
- return (
-   <div className="jobcontainer-grid">
-    <h2 style={{textAlign:"center"}}>All Jobs</h2>
-     <div className="jobcontainer">
-       {jobApplications.map((job, index) => (
-         <JobItem key={index} job={job} />
-       ))}
-     </div>
-   </div>
- );
+  return (
+    <div className="jobcontainer-grid">
+      <h2 style={{ textAlign: "center" }}>All Jobs</h2>
+      <div className="jobcontainer">
+        {jobApplications.map((job, index) => (
+          <JobItem key={index} job={job} idx={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default JobList;
