303df884d393c052e5c03aa8a81f140e10103357
Erick559 <erickungu82@gmail.com>
2024-02-27 11:45:47 +0300

    feat: Add a classname main to the about component.

--- src/components/layout/About.jsx
+++ src/components/layout/About.jsx
@@ -1,6 +1,6 @@
 function About () {
   return (
-      <div>
+      <div className="main">
         <h2>About</h2>
         <p>JobTrackr is created to keep track of your job applications. Made with love. &#x1F497;</p>
       </div>
