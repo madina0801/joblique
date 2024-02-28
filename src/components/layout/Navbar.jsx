ca7078557dd1f7ad2646e1618f136f7b31ec3f4f
Madina <madinatussupova0820@gmail.com>
2024-02-28 16:57:56 +0600

    change navbar to tailwind

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
    >
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
      <a
-      href="https://flowbite.com/"
+      href="/"
       className="flex items-center space-x-3 rtl:space-x-reverse"
      >
       <img src={logo} className="h-8" alt="Flowbite Logo" />
@@ -34,7 +34,7 @@ function Navbar() {
       <span className="sr-only">Open main menu</span>
       {isMobileMenuOpen ? <IoClose /> : <IoMenu />}
      </button>
-     <div className={`w-full md:block md:w-auto ${!isMobileMenuOpen ? "hidden" : ""}`} id="navbar-default">
+     <div className={`w-full md:block md:w-auto ${!isMobileMenuOpen ? "hidden" : "nav-mobile"}`} id="navbar-default">
       <ul className="text-darkblue font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
        <NavLink
         to="/"
