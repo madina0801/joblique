52f6969b4694467ac710c19258ca7b37a49a068f
Madina <madinatussupova0820@gmail.com>
2024-02-29 12:44:33 +0600

    navbar in tailiwind (prettier)

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -13,9 +13,9 @@ function Navbar() {
 
  const content = (
   <>
-   <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-darkblue text-light transition z-50">
+   <div className="lg:hidden md:hidden block absolute top-16 w-full left-0 right-0 bg-darkblue text-light transition z-50">
     <ul className="text-center text-xl p-20">
-     <NavLink to="/"  onClick={toggleMobileMenu}>
+     <NavLink to="/" onClick={toggleMobileMenu}>
       <li className="hover:text-lightblue transition border-b0  border-darkblue hover:border-lightblue cursor-pointer">
        Home
       </li>
@@ -25,7 +25,11 @@ function Navbar() {
        About
       </li>
      </NavLink>
-     <NavLink to="https://github.com/madina0801/JobTrackr" target="_blank"  onClick={toggleMobileMenu}>
+     <NavLink
+      to="https://github.com/madina0801/JobTrackr"
+      target="_blank"
+      onClick={toggleMobileMenu}
+     >
       <li className="hover:text-lightblue transition border-b0  border-darkblue hover:border-lightblue cursor-pointer">
        GitHub
       </li>
@@ -50,26 +54,27 @@ function Navbar() {
     <button
      data-collapse-toggle="navbar-default"
      type="button"
-     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
      aria-controls="navbar-default"
      aria-expanded="false"
+     onClick={toggleMobileMenu}
     >
-     <button onClick={toggleMobileMenu}>
-      {isMobileMenuOpen ? <IoClose /> : <IoMenu />}
-     </button>
+     {isMobileMenuOpen ? (
+      <IoClose className="w-10 h-10" />
+     ) : (
+      <IoMenu className="w-10 h-10" />
+     )}
     </button>
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-     <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <NavLink to="/" className="main-nav-link">
-       Home
+     <ul class="font-medium text-darkblue flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <NavLink to="/">
+       <li className="nav-link">Home</li>
       </NavLink>
-      <NavLink to="/about" className="main-nav-link">
-       About
+      <NavLink to="/about">
+       <li className="nav-link">About</li>
       </NavLink>
       <NavLink to="https://github.com/madina0801/JobTrackr" target="blank">
-       <a className="main-nav-link">
-        GitHub
-       </a>
+       <a className="nav-link">GitHub</a>
       </NavLink>
      </ul>
     </div>
@@ -77,74 +82,6 @@ function Navbar() {
 
    <div>{isMobileMenuOpen && content}</div>
   </nav>
-  //   <header
-  //    className={`header  ${isMobileMenuOpen ? "nav-open" : ""}`}
-  //    style={{
-  //     zIndex: "999",
-  //     position: "sticky",
-  //     top: "0",
-  //     backdropFilter: "blur(10px)",
-  //     backgroundColor: "rgba(255, 255, 255, 0.7)",
-  //    }}
-  //   >
-  //    <div className="logo">
-  //     {" "}
-  //     <a href="#">
-  //      <img
-  //       className="logo"
-  //       alt="JobTrackr logo"
-  //       src={logo}
-  //       style={{
-  //        height: "4.5rem",
-  //        width: "3rem",
-  //        position: "absolute",
-  //        top: "50%",
-  //        transform: "translateY(-50%)",
-  //       }}
-  //      />
-  //     </a>
-  //    </div>
-  //    <div
-  //     className="heading-primary text-darkblue"
-  //     style={{ position: "absolute", left: "50%", transform: "translateX(-50%)" }}
-  //    >
-  //     JobTrackr
-  //    </div>
-  //    <nav
-  //     className={`main-nav ${isMobileMenuOpen ? "open" : ""}`}
-  //     style={{ zIndex: "300" }}
-  //    >
-  //     <ul className="main-nav-list">
-  //      <NavLink to="/" className="main-nav-link" onClick={toggleMobileMenu}>
-  //       Home
-  //      </NavLink>
-  //      <NavLink to="/about" className="main-nav-link" onClick={toggleMobileMenu}>
-  //       About
-  //      </NavLink>
-  //      <li>
-  //       <a
-  //        className="main-nav-link"
-  //        target="blank"
-  //        href="https://github.com/madina0801/JobTrackr"
-  //        onClick={toggleMobileMenu}
-  //       >
-  //        GitHub
-  //       </a>
-  //      </li>
-  //     </ul>
-  //    </nav>
-  //    <button
-  //     className="btn-mobile-nav"
-  //     onClick={toggleMobileMenu}
-  //    >
-  //     {isMobileMenuOpen ? <IoClose className="icon-mobile-nav" /> : <IoMenu className="icon-mobile-nav" />}
-  //     {/* <ion-icon
-  //      className="icon-mobile-nav"
-  //      name={isMobileMenuOpen ? "close-outline" : "menu-outline"}
-  //     ></ion-icon> */}
-  //    </button>
-  //   </header>
-  //  );
  );
 }
 
