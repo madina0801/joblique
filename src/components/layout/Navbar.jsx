ee0ed89a93f1db73e015816b5f5d495230f18407
Madina <madinatussupova0820@gmail.com>
2024-02-29 12:59:11 +0600

    navbar full width

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -40,17 +40,16 @@ function Navbar() {
  );
 
  return (
-  <nav className="bg-white border-gray-200 dark:bg-gray-900">
+  <nav className="w-screen">
    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a
-     href="https://flowbite.com/"
+    <div
      class="flex items-center space-x-3 rtl:space-x-reverse"
     >
      <img src={logo} class="h-8" alt="JobTrackr Logo" />
-     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+     <span class="self-center text-2xl text-darkblue font-semibold whitespace-nowrap dark:text-white">
       JobTrackr
      </span>
-    </a>
+    </div>
     <button
      data-collapse-toggle="navbar-default"
      type="button"
