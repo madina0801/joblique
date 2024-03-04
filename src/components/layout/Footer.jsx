dfe0d1a7679dbc752d5274d61ed86c927cc63887
Madina <madinatussupova0820@gmail.com>
2024-03-04 14:29:56 +0500

    update footer styles

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
 	return (
-		<div className="bg-darkblue">
+		<div className="bg-darkblue text-lightblue w-screen text-center mt-auto p-6">
 			JobTrackr 2024 &copy; Madina Tusupova
 		</div>
 	)
