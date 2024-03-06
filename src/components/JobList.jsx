1fc53830a3345f26e417e5ed2749293242cd6cba
Calen Cooper <CalenCoop1@gmail.com>
2024-03-05 22:28:24 -0800

    added conditional logic to job application div

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -3,11 +3,19 @@ import JobItem from "./JobItem";
 
 function JobList({ jobApplications, setJobApplications }) {
 
+  const [savedJobs, setSavedJobs] = useState([])
+  useEffect(() =>{
+    const storedJobs= JSON.parse(localStorage.getItem('jobApplications')) || []
+    setSavedJobs(storedJobs)
+  },[])
+  
  return (
   <div className="jobcontainer">
+    {jobApplications.length > 0 && (
     <div className="flex justify-center text-xl font-bold text-darkblue mt-5">
      <h2 className="">All Jobs</h2>
     </div>
+    )}
    {jobApplications.map((job, index) => (
     <JobItem
      key={index}
