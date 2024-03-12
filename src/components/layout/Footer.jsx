e707bc4ab0fef81ad1bd5ac15347d6b4455e9bb3
Madina <madinatussupova0820@gmail.com>
2024-03-12 09:22:31 +0500

    style footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
  return (
-  <footer className="sticky top-[100vh]">
+  <footer className="sticky w-screen bg-darkblue text-light p-5 text-center">
    Job Trackr 2024 &copy; Madina Tussupova
   </footer>
  );
