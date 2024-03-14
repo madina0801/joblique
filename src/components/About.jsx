cbb1fda012083900ee25149258a9f151e115ab52
Madina <madinatussupova0820@gmail.com>
2024-03-14 13:25:03 +0500

    update About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,17 @@
-function About () {
-  return (
-      <div className="h-screen">
-        <h2>About</h2>
-        <p>JobTrackr is created to keep track of your job applications. Made with love. &#x1F497;</p>
-      </div>
-  );
-};
+function About() {
+ return (
+  <div className="h-screen flex flex-col items-center text-darkblue">
+   <h2 className="text-xl font-semibold">About</h2>
+   <p className="text-md px-20 mt-3 text-justify">
+    I wanted to create simple and effective application to keep track of jobs
+    I've applied for. And decided to share it with everyone. JobTrackr is an
+    open-source project. So, everyone can contribute, I review code changes and
+    the contributors can see their changes live. I aim to make this community
+    friendly and inviting. <br />
+    Created with love. &#x1F497;
+   </p>
+  </div>
+ );
+}
 
 export default About;
