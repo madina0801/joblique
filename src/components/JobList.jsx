c89a4ffd3dbb0cdb9e01ad6cfeeb3c2a4b5b9452
Madina <madinatussupova0820@gmail.com>
2024-03-14 09:06:24 +0500

    make "All Jobs" title bigger

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -12,12 +12,12 @@ function JobList({ jobApplications, setJobApplications }) {
  return (
   <div>
    {jobApplications.length > 0 && (
-    <div className="flex justify-center text-xl font-bold text-darkblue mt-5">
-     <h2 className="">All Jobs</h2>
+    <div className="flex justify-center text-xl font-bold text-darkblue mt-10">
+     <h2 className="text-2xl">All Jobs</h2>
     </div>
    )}
    <div className="flex justify-center m-10">
-    <div className="grid gap-5 grid-cols-1 md:grid-cols-2">
+    <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
      {jobApplications.map((job, index) => (
       <JobItem
        key={index}
