66e615e5a450cf57af47db44fc30a6343db08016
Madina <madinatussupova0820@gmail.com>
2024-03-24 15:17:40 +0500

    update navbar styling

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -66,30 +66,30 @@ function Navbar() {
      <button
       data-collapse-toggle="navbar-default"
       type="button"
-      className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:text-light dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      className="inline-flex items-center p-2 text-darkblue justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-lightblue dark:text-lightblue"
       aria-controls="navbar-default"
       aria-expanded="false"
       onClick={toggleMobileMenu}
      >
       {isMobileMenuOpen ? (
-       <IoClose className="w-12 h-12" />
+       <IoClose className="w-7 h-7" />
       ) : (
-       <IoMenu className="w-12 h-12" />
+       <IoMenu className="w-7 h-7" />
       )}
      </button>
     </div>
 
     <div className="flex items-center">
      <div className="hidden md:block mr-4">
-      <ul className="font-medium text-darkblue flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+      <ul className="font-medium text-darkblue flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:text-light">
        <NavLink to="/">
-        <li className="nav-link  dark:text-light">Home</li>
+        <li className="hover:text-lightblue">Home</li>
        </NavLink>
        <NavLink to="/about">
-        <li className="nav-link  dark:text-light">About</li>
+        <li className="hover:text-lightblue">About</li>
        </NavLink>
        <NavLink to="https://github.com/madina0801/JobTrackr" target="blank">
-        <li className="nav-link  dark:text-light">GitHub</li>
+        <li className="hover:text-lightblue">GitHub</li>
        </NavLink>
       </ul>
      </div>
