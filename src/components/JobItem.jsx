343f1dc129208adf18077ce3ada57f2a21cc080f
Madina <madinatussupova0820@gmail.com>
2024-03-26 15:04:39 +0500

    fix styles

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -28,9 +28,9 @@ function JobItem({ job, idx, setJobApplications }) {
  };
 
  return (
-  <div className="bg-light border border-darkblue text-darkblue w-fit p-7 rounded-lg shadow-xl shadow-lightblue
-     hover:shadow-[#00ff00]">
-        {/* if tailwind was working properly then it would be written like this hover:shadow-green-200 */}
+  <div
+   className="w-[300px] bg-light border border-darkblue text-darkblue p-7 rounded-lg shadow-xl shadow-lightblue hover:shadow-[#00ff00]"
+  >
    <div className="flex space-x-7 mb-7">
     <div className="flex items-center text-sm">
      <div>
@@ -45,14 +45,8 @@ function JobItem({ job, idx, setJobApplications }) {
      </div>
      <span>{job.company}</span>
     </div>
-   
-   <div id="delete-btn" className="text-sm">
-     <span onClick={handleDeleteJob}>
-      <IoTrashBin className="h-6 w-6 mr-1" />
-      Delete
-     </span>
-    </div>
-    </div>
+   </div>
+
    <div className="flex space-x-7 mb-7">
     <div className="flex items-center text-sm">
      <div>
