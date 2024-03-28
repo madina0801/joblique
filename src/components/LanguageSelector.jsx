1d0cc979005c4be0f6fc6b5fbe3f4b546b420299
Madina <madinatussupova0820@gmail.com>
2024-03-28 15:41:26 +0500

    prettier card

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
 const LanguageSelector = ({ onSelectLanguage }) => {
  const { i18n } = useTranslation();
+
  const [showOptions, setShowOptions] = useState(false);
- 
+
  const languages = [
-  { code: "en", name: "English"},
-  { code: "ru", name: "Russian" },
- ]; 
+  { code: "en", name: "English", flagCode: "gb" },
+  { code: "ru", name: "Russian", flagCode: "ru" },
+ ];
 
  const handleLanguageSelect = (languageCode) => {
   i18n.changeLanguage(languageCode);
@@ -16,25 +19,31 @@ const LanguageSelector = ({ onSelectLanguage }) => {
   localStorage.setItem("lng", languageCode);
  };
 
+ const selectedLang = languages.find(
+  (language) => language.code === i18n.language
+ );
+
  return (
-  <div className="relative w-20">
-   <div className="relative">
+  <div className="relative w-[120px]">
+   <div className="relative shadow-lg">
     <button
-     className="px-1 w-full bg-lightgray border border-gray-300 bg-white cursor-pointer text-left"
+     className={`px-1 w-full bg-light border border-lightblue cursor-pointer text-left ${showOptions ? "rounded-t" : "rounded"} `}
      id="openButton"
      onClick={() => setShowOptions(!showOptions)}
     >
-     {i18n.language.toUpperCase()}
+     {selectedLang.name}
+     <span className={`ml-5 fi fi-${selectedLang.flagCode}`}></span>
     </button>
     {showOptions && (
-     <div className="absolute bg-lightgray top-full left-0 z-50 w-full border border-gray-300 text-sm">
-      {languages.map((language) => (
+     <div className="absolute bg-light">
+      {languages.map((language, inx) => (
        <button
-        className="w-full px-1 py-2 border-b border-gray-300 hover:bg-gray-100 cursor-pointer text-left z-50"
+        className={`w-full px-1 py-2 shadow-lg border-b border-lightblue hover:bg-gray-100 cursor-pointer text-left z-50 ${inx % 2 === 0 ? 'rounded-r' : 'rounded-l'}`}
         key={language.code}
         onClick={() => handleLanguageSelect(language.code)}
        >
         {language.name}
+        <span className={`ml-5 fi fi-${language.flagCode}`}></span>
        </button>
       ))}
      </div>
