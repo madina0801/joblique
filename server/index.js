9467f03391776af386b412f1ca67e97c4ed0fcd3
Madina <madinatussupova0820@gmail.com>
2024-04-08 09:23:51 +0500

    set up cors

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
+
+app.use(cors({
+  origin: 'http://localhost:5187',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`);
