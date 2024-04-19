ba61e84459b40ec7164ee2d96cbb11d323df010d
Madina <madinatussupova0820@gmail.com>
2024-04-19 12:28:30 +0500

    move the title out of the JobList

--- client/src/components/JobApp.jsx
+++ client/src/components/JobApp.jsx
@@ -1,8 +1,11 @@
+import { useTranslation } from "react-i18next";
+
 import { useState, useEffect } from "react";
 import JobForm from "./JobForm";
 import JobList from "./JobList";
 
 function JobApp() {
+ const { t } = useTranslation();
  const [jobApplications, setJobApplications] = useState([]);
 
  useEffect(() => {
@@ -17,6 +20,11 @@ function JobApp() {
  return (
   <div className="bg-lightgray dark:bg-darkblue">
    <JobForm onAddJob={addJobApplication} />
+   {jobApplications.length > 0 && (
+    <div className="flex justify-center text-xl font-bold text-darkblue mt-10 dark:text-light">
+     <h2 className="text-2xl">{t("jobsTitle")}</h2>
+    </div>
+   )}
    <JobList
     jobApplications={jobApplications}
     setJobApplications={setJobApplications}
--- client/src/components/JobList.jsx
+++ client/src/components/JobList.jsx
@@ -13,11 +13,6 @@ function JobList({ jobApplications, setJobApplications }) {
 
  return (
   <div className="h-screen bg-lightgray dark:bg-darkblue">
-   {jobApplications.length > 0 && (
-    <div className="flex justify-center text-xl font-bold text-darkblue mt-10 dark:text-light">
-     <h2 className="text-2xl">{t("jobsTitle")}</h2>
-    </div>
-   )}
    <div className="flex justify-center m-10">
     <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
      {jobApplications.map((job, index) => (
